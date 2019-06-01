50faffe8d395567b970aecaa5da663242c1d88b7
yeshwanth32 <40725768+yeshwanth32@users.noreply.github.com>
2019-06-01 16:00:14 -0400

    Comments written and unnecessary system outs removed.

--- src/Driver.java
+++ src/Driver.java
@@ -25,7 +25,7 @@ import java.util.List;
 import java.nio.file.Files;
 
 // class description
-// this is the Main Driver, the place where all the magic happens.
+// this is the Main Driver.
 public class Driver {
     private static int Action;
     private static boolean ExitTrain;
